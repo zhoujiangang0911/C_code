c54ddfa16d8ec01316c5f34f7b6b4a3c85113dfd
zhoujg77 <zhoqjg77@gmail.com>
2015-01-24 18:52:14 +0800

    1

---
+++ code/b.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int x =1,y=2;
+	printf("x=%d y=%d sum = %d\n",x,y,x+y);
+	printf ("10 Squared is :%d\n",10*10);
+	
+	return 0;
+
+}
+
---
+++ code/d.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	char ch;
+	int i;
+	float f;
+	double d;
+	ch ='A';
+	i=1;
+	f=2.0;
+	d=2.5;
+	printf("%f",ch/i+f*d-(f+i));
+	
+	
+	
+	return 0;
+}
